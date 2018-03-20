ac4a27382214e2567b6c12ac94b3a98b8065ecd0
yeonghoey <yeonghoey@gmail.com>
2018-03-20 19:44:26 +0900

    Working on auth

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask.json import jsonify
+import requests
 
 
 app = Flask('galpi')
 
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html',
+                           client_id=CLIENT_ID,
+                           redirect_uri='http://localhost:5000/auth')
+
+
+@app.route("/auth")
+def auth():
+    url = 'https://github.com/login/oauth/access_token'
+    headers = {
+        'Accept': 'application/json',
+    }
+    payload = {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'code': request.args['code'],
+    }
+
+    res = requests.post(url, headers=headers, data=payload)
+    return jsonify(res.json())
