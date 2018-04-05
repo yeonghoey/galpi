113fc6f3075ff640cc59fd384ef8066c9e760521
Yeongho Kim <yeonghoey@gmail.com>
2018-04-06 01:30:52 +0900

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <h1 class="w-75 px-2 py-3 mx-auto">
+      Galpi
+    </h1>
     <router-view
       class="w-75 m-auto"
       :key="$route.path"
@@ -18,12 +21,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
 }
-
-.view {
-  max-width: 960px;
-  margin: 0 auto;
-}
-
 </style>
