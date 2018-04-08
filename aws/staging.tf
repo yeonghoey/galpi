5d4ecf650e4bc000557409ef0978e7e66c6994d8
Yeongho Kim <yeonghoey@gmail.com>
2018-04-08 22:49:50 +0900

    Remove client_staging

--- aws/staging.tf
+++ aws/staging.tf
@@ -1,10 +1,3 @@
-module "client_staging" {
-  source              = "./client"
-  bucket              = "galpi-staging"
-  domains             = ["staging.galpi.io"]
-  acm_certificate_arn = "${aws_acm_certificate.app.arn}"
-}
-
 module "params_staging" {
   source    = "./params"
   stage     = "staging"
