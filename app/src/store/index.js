c4aad794ecb6c7f7f9cfc562c5893690a15c6269
Yeongho Kim <yeonghoey@gmail.com>
2018-04-13 10:59:24 +0900

    Apply vuex-persistedstate

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 import auth from '@/store/modules/auth';
 
@@ -7,7 +8,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
-
+  plugins: [createPersistedState()],
   modules: {
     auth,
   },
