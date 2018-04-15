0dc0772e7251a39d5c60cab8e1e60525b7be8332
Yeongho Kim <yeonghoey@gmail.com>
2018-04-15 20:13:26 +0900

    Rename _variables.scss to resources.scss

--- app/build/utils.js
+++ app/build/utils.js
@@ -64,7 +64,7 @@ exports.cssLoaders = function (options) {
       {
         loader: 'sass-resources-loader',
         options: {
-          resources: path.resolve(__dirname, '../src/assets/scss/_variables.scss'),
+          resources: path.resolve(__dirname, '../src/assets/scss/resources.scss'),
         },
       }),
     stylus: generateLoaders('stylus'),
