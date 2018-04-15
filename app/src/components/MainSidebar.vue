0d869d1d1dd5a6c0b3fccd379463c569137d0b95
Yeongho Kim <yeonghoey@gmail.com>
2018-04-15 20:13:47 +0900

    Add MainView

---
+++ app/src/components/MainSidebar.vue
@@ -0,0 +1,59 @@
+<template>
+  <aside
+    class="d-flex flex-column p-2 bg-light">
+    <div class="flex-shrink-0 mb-1 p-2">
+      <h2 class="text-center">
+        <b-link
+          class="text-secondary"
+          :to="`/${username}`"
+          >
+          {{ username }}
+        </b-link>
+      </h2>
+      <hr>
+      <b-button
+        class="mx-auto w-75"
+        size="sm"
+        variant="primary"
+        block
+        >
+        New
+      </b-button>
+    </div>
+
+    <div class="p-2 overflow-y-auto">
+      <b-list-group>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+        <b-list-group-item>Test</b-list-group-item>
+      </b-list-group>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'MainSidebar',
+  props: {
+    username: String,
+  },
+};
+</script>
---
+++ app/src/views/MainView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex align-items-stretch">
+    <MainSidebar
+      class="main-sidebar"
+      :username="username"
+    />
+  </div>
+</template>
+
+<script>
+import MainSidebar from '@/components/MainSidebar';
+
+export default {
+  name: 'MainView',
+
+  props: {
+    username: String,
+  },
+
+  components: {
+    MainSidebar,
+  },
+};
+</script>
+
+<style scoped>
+.main-sidebar {
+  flex: 0 1 240px;
+}
+</style>
