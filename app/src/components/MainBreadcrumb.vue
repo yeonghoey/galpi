c04ad3a835c3e81c21b3b39b1fddc60cc1598e56
yeonghoey <yeonghoey@gmail.com>
2018-04-18 16:51:42 +0900

    Implement MainBreadcrumb

---
+++ app/src/components/MainBreadcrumb.vue
@@ -0,0 +1,72 @@
+<template>
+  <b-breadcrumb class="align-items-center">
+    <b-breadcrumb-item
+      v-for="(item, index) in items"
+      :key="index"
+      :active="index === items.length-1"
+      :class="index === 0 ? classUser : []"
+      v-bind="item"
+      />
+    <b-button
+      v-if="owner"
+      class="ml-auto"
+      >
+      New
+    </b-button>
+  </b-breadcrumb>
+</template>
+
+<script>
+import _ from 'lodash';
+
+export default {
+  name: 'MainBreadcrumb',
+
+  props: {
+    owner: {
+      type: Boolean,
+      required: true,
+    },
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    classUser() {
+      return { user: true, owner: this.owner };
+    },
+    items() {
+      const base = _.get(this.item, 'base', []);
+      const paths = _.transform(base, (ps, s) => {
+        ps.push(`${_.last(ps) || ''}/${s}`);
+      }, []);
+
+      const result = _.zip(base, paths).map((pair) => {
+        const [b, p] = pair;
+        // Make sure that navigating always gives list,
+        // not direct redirection
+        return { text: b, to: p };
+      });
+
+      return result;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.user {
+  font-size: $font-size-lg;
+  font-weight: $font-weight-bold;
+}
+
+.owner::after {
+  margin-left: 0.1rem;
+  content: "(you)";
+  color: $secondary;
+  font-size: $font-size-sm;
+  font-weight: $font-weight-light;
+}
+</style>
--- app/src/views/MainView.vue
+++ app/src/views/MainView.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    {{ user }} / {{ path }}
+    <MainBreadcrumb
+      :owner="owner"
+      :item="item"
+      />
+    {{ item }}
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import api from '@/api';
+import MainBreadcrumb from '@/components/MainBreadcrumb';
+
 export default {
   name: 'MainView',
 
@@ -15,7 +23,37 @@ export default {
     },
     path: {
       type: String,
+      default: '',
     },
+  },
+
+  components: {
+    MainBreadcrumb,
+  },
+
+  data() {
+    return {
+      item: {},
+    };
+  },
+
+  computed: {
+    owner() {
+      return this.user === this.me;
+    },
+    ...mapState('auth', ['me']),
+  },
+
+  created() {
+    const url = `/${this.user}/${this.path}`;
+    api.get(url).then((response) => {
+      const link = response.data.link;
+      if (link) {
+        window.location.href = link;
+      } else {
+        this.item = response.data;
+      }
+    });
   },
 };
 </script>
