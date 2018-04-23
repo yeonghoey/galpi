38160954b2be1ba2caa6692e064a5f35f1d631a9
Yeongho Kim <yeonghoey@gmail.com>
2018-04-23 15:48:20 +0900

    Apply prod params

---
+++ aws/prod.tf
@@ -0,0 +1,5 @@
+module "params_prod" {
+  source    = "./params"
+  stage     = "prod"
+  client_id = "e6fdc6f4954340579afe"
+}
--- aws/staging.tf
+++
@@ -1,5 +0,0 @@
-module "params_staging" {
-  source    = "./params"
-  stage     = "staging"
-  client_id = "fd7ab9a0164759ebf50f"
-}
