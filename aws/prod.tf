c721084570ff26f5665cccb6438598917ea24841
yeonghoey <yeonghoey@gmail.com>
2018-05-11 17:27:40 +0900

    Rename module addresses in aws

--- aws/prod.tf
+++ aws/prod.tf
@@ -1,12 +1,12 @@
-module "params_prod" {
-  source    = "./params"
-  stage     = "prod"
-  client_id = "e6fdc6f4954340579afe"
-}
-
-module "client_prod" {
+module "prod_client" {
   source              = "./client"
   bucket              = "galpi-prod"
   domains             = ["galpi.io", "www.galpi.io"]
   acm_certificate_arn = "${aws_acm_certificate.app.arn}"
+}
+
+module "prod_params" {
+  source    = "./params"
+  stage     = "prod"
+  client_id = "e6fdc6f4954340579afe"
 }
