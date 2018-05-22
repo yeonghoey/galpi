3d2d37c0bbd931b10451841d229f95c36f9c3bb1
yeonghoey <yeonghoey@gmail.com>
2018-05-23 01:48:20 +0900

    Add wait when init tables

--- api/galpi/db/tables.py
+++ api/galpi/db/tables.py
@@ -10,10 +10,13 @@ def table(name):
         if not hasattr(g, attr):
             prefix = current_app.config['TABLE_PREFIX']
             table = db.Table(f'{prefix}-{name}')
+            # NOTE: `table.wait_until_exists()` waits 20 seconds by default
+            # and expose no parameter. Fall back to the raw client's waiter.
+            waiter = db.meta.client.get_waiter('table_exists')
+            waiter.wait(TableName=table.name, WaiterConfig={'Delay': 5})
             setattr(g, attr, table)
         return getattr(g, attr)
     return f
 
 
 items = LocalProxy(table('items'))
-users = LocalProxy(table('users'))
