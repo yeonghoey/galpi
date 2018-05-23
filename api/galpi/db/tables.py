04a12015c2175d14417c90e4bbd9f44fbb932872
Yeongho Kim <yeonghoey@gmail.com>
2018-05-23 10:49:21 +0900

    Check table exists only IS_OFFLINE is True

--- api/galpi/db/tables.py
+++ api/galpi/db/tables.py
@@ -10,13 +10,21 @@ def table(name):
         if not hasattr(g, attr):
             prefix = current_app.config['TABLE_PREFIX']
             table = db.Table(f'{prefix}-{name}')
-            # NOTE: `table.wait_until_exists()` waits 20 seconds by default
-            # and expose no parameter. Fall back to the raw client's waiter.
-            waiter = db.meta.client.get_waiter('table_exists')
-            waiter.wait(TableName=table.name, WaiterConfig={'Delay': 5})
             setattr(g, attr, table)
+
+            # NOTE: Local DB takes time to create tables.
+            # In production, they are already there, so skip this.
+            if current_app.config['IS_OFFLINE']:
+                wait_until_exists(table, 1)
         return getattr(g, attr)
     return f
+
+
+def wait_until_exists(table, seconds):
+    # NOTE: `table.wait_until_exists()` waits 20 seconds by default
+    # and expose no parameter. Fall back to the raw client's waiter.
+    waiter = db.meta.client.get_waiter('table_exists')
+    waiter.wait(TableName=table.name, WaiterConfig={'Delay': seconds})
 
 
 items = LocalProxy(table('items'))
